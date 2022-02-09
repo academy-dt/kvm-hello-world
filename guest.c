1275882338d955a8fc7907b620125fd1684ce9ef
Daniel Trugman <dtrugman@gmail.com>
2022-02-09 21:16:29 +0000

    Add in/out methods

--- guest.c
+++ guest.c
@@ -1,15 +1,21 @@
 #include <stddef.h>
 #include <stdint.h>
 
-static void outb(uint16_t port, uint8_t value) {
-	asm("outb %0,%1" : /* empty */ : "a" (value), "Nd" (port) : "memory");
+static inline uint32_t inb(uint16_t port) {
+	uint32_t ret;
+	asm("in %1, %0" : "=a"(ret) : "Nd"(port) : "memory" );
+	return ret;
+}
+
+static inline void outb(uint16_t port, uint32_t value) {
+	asm("out %0,%1" : /* empty */ : "a" (value), "Nd" (port) : "memory");
 }
 
 void
 __attribute__((noreturn))
 __attribute__((section(".start")))
 _start(void) {
-	const char *p;
+	const char *p = "Hello, world!\n";
 
 	for (p = "Hello, world!\n"; *p; ++p)
 		outb(0xE9, *p);
